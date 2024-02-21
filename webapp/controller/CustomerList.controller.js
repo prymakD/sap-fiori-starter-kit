4488a402b996ce5cd86897a64bf824780441342c
Danila Prymak <prymakdn@gmail.com>
2024-02-21 14:49:12 +0000

    added searchField by country on CustomerList view

--- webapp/controller/CustomerList.controller.js
+++ webapp/controller/CustomerList.controller.js
@@ -1,13 +1,18 @@
-sap.ui.define(
-  ["sap/ui/core/mvc/Controller"],
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator"
+],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller) {
+  function (Controller, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("stk.starterkit.controller.CustomerList", {
-      onInit: function () {},
+      onInit: function () {
+        this.oSF = this.getView().byId("searchField");
+      },
 
       onCustomerPress: function (oEvent) {
         var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
@@ -21,10 +26,45 @@ sap.ui.define(
         var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
         oRouter.navTo("CreateCustomer");
     },
+
       onPressPerformance: function (){
         var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
         oRouter.navTo("CustomerPerformance");
-      } 
+    }, 
+
+    onSearch : function (oEvent) {
+
+      var aFilter = [];
+      var sQuery = oEvent.getParameter("query");
+      if (sQuery) {
+        aFilter.push(new Filter("Country", FilterOperator.Contains, sQuery));
+      }
+
+      var oList = this.byId("customersTable");
+      var oBinding = oList.getBinding("items");
+      oBinding.filter(aFilter);
+    },
+
+    onSuggest: function (oEvent) {
+      debugger
+			var sValue = oEvent.getParameter("suggestValue"),
+				aFilters = [];
+			if (sValue) {
+				aFilters = [
+					new Filter([
+						new Filter("CustomerID", function (sText) {
+							return (sText || "").toUpperCase().indexOf(sValue.toUpperCase()) > -1;
+						}),
+						new Filter("Country", function (sDes) {
+							return (sDes || "").toUpperCase().indexOf(sValue.toUpperCase()) > -1;
+						})
+					], false)
+				];
+			}
+
+			this.oSF.getBinding("suggestionItems").filter(aFilters);
+			this.oSF.suggest();
+		}
     
     });
   }
